722357b3d6575ddce28eecbc00c0683ccc7fccba
Sanil <sanilaadityan@gmail.com>
2025-06-09 22:04:37 -0700

    Create Syntax practice.jl
    this just a sample julia code im still Learning  julia lol. just testing out the syntax of julia

---
+++ Syntax practice.jl
@@ -0,0 +1,26 @@
+
+using Plots
+
+# Genetic.jl
+# This file is part of the Genetic.jl package.
+println("hello from Genetic.jl")
+
+function £(x)
+    if x > 5 
+        println("x is sigma boy")
+    else 
+        println(5)
+    end
+
+end
+
+£(10)
+
+
+G=(5,"joe biden",true,1,2,3,5.5)
+println(G[2])
+1+1 
+using Plots
+# Plotting a simple function
+plot(x->x^2,0,10)
+plot( csv)
